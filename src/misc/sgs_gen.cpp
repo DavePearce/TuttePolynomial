b0cabcf962c9e26121a989c9d35cbf706382edca
DavePearce <david.pearce@ecs.vuw.ac.nz>
2007-01-15 21:41:08 +0000

    Graph generator is working now, although I need to test it.
    git-svn-id: svn+ssh://circa.ecs.vuw.ac.nz/u/staff/djp/projects/repository/tuttepoly/trunk@825 cab769db-1af3-0310-8bf3-f2d376eea66d

--- src/misc/sgs_gen.cpp
+++ src/misc/sgs_gen.cpp
@@ -1,5 +1,4 @@
-//
-// This is the small graph generator, which automatically generates
+// This is the Small Graph Solution Generator, which automatically generates
 // code for matching graphs of a particular size.
 
 #include "nauty.h"
@@ -10,10 +9,16 @@
 #include <ext/hash_set>
 #include <cmath>
 #include "../graph/adjacency_list.hpp"
+#include "../graph/spanning_graph.hpp"
 #include "../graph/algorithms.hpp"
+#include "../poly/simple_poly.hpp"
 
 using namespace std;
 using namespace __gnu_cxx;
+
+typedef adjacency_list<> graph_t;
+typedef spanning_graph<adjacency_list<> > sgraph_t;
+typedef simple_poly poly_t;
 
 vector<vector<int> > combinator(int N, int min, int max) {
   bool finished;
@@ -48,7 +53,7 @@ vector<vector<int> > combinator(int N, int min, int max) {
 
 
 // this function recursively generates the list of all possible graphs
-void generate_all_graphs(int v, int V, adjacency_list<> graph, list<adjacency_list<> > &graphs) {  
+void generate_all_graphs(int v, int V, graph_t graph, list<graph_t> &graphs) {  
   if(v == (V-1)) {
     // this is the base case --- there are no more edges to add!
     graphs.push_back(graph);    
@@ -59,7 +64,7 @@ void generate_all_graphs(int v, int V, adjacency_list<> graph, list<adjacency_li
       for(int i=0;i!=combs.size();++i) {
 	vector<int> const &vec(combs[i]);
 	// create graph for this combination
-	adjacency_list<> g(graph);
+	graph_t g(graph);
 	for(vector<int>::const_iterator j(vec.begin());j!=vec.end();++j) {
 	  g.add_edge(v,*j); 
 	}
@@ -80,7 +85,7 @@ public:
   }
 };
 
-vector<int> degree_sequence(adjacency_list<> const &graph) {
+vector<int> degree_sequence(graph_t const &graph) {
   vector<int> r(graph.num_vertices());
   for(int i=0;i!=r.size();++i) { r[i] = 0; }
 
@@ -91,7 +96,7 @@ vector<int> degree_sequence(adjacency_list<> const &graph) {
   return r;
 }
 
-unsigned long degree_sequence_key(adjacency_list<> const &graph, int shift) {
+unsigned long degree_sequence_key(graph_t const &graph, int shift) {
   unsigned long key = 0;
 
   for(int i=0;i!=graph.num_vertices();++i) { 
@@ -101,13 +106,49 @@ unsigned long degree_sequence_key(adjacency_list<> const &graph, int shift) {
   return key;
 }
 
-bool two_connected(adjacency_list<> const &g) {
+bool two_connected(graph_t const &g) {
   for(int i=0;i!=g.num_vertices();++i) {
     if(g.num_edges(i) < 2) {
       return false;
     }
   }
   return true;
+}
+
+
+// a simple tutte polynomial solver which doesn't do anything fancy
+poly_t delete_contract(sgraph_t &g) { 
+  if(g.is_looptree()) {
+    return poly_t(g.num_edges()-g.num_loops(),g.num_loops());
+  } else {
+    // Perform delete contract 
+    pair<int,int> e = g.select_nontree_edge();
+
+    sgraph_t g1(g);  
+    g1.remove_edge(e.first,e.second);        
+    sgraph_t g2(g1); 
+    g2.contract_edge(e.first,e.second); 
+
+    // Fourth, recursively compute the polynomial
+    return delete_contract(g1) + delete_contract(g2); // perform the recursion
+  }    
+}
+
+void print_graph_poly(graph_t const &graph) {
+  // this function has got to compute the solution for the given small
+  // graph and then print it as as a series of polynomial constructs.
+  
+  // first, construct solution
+  sgraph_t g(graph);
+  poly_t r = delete_contract(g);
+  
+  // second, print it!
+  int count=0;
+  for(poly_t::const_iterator i(r.begin());i!=r.end();++i,++count) {
+    if(count == 6) { cout << endl << "\t\t\t\t"; count = 0; }
+    if(i!=r.begin()) { cout << " + "; }
+    cout << "P(" << i->second << "," << i->first.xpower << "," << i->first.ypower << ")";
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -118,21 +159,21 @@ int main(int argc, char *argv[]) {
   try {
     
     // First, we generate the set of all unique graphs
-    list<adjacency_list<> > graphs;
-    generate_all_graphs(0,size, adjacency_list<>(size), graphs);  
+    list<graph_t> graphs;
+    generate_all_graphs(0,size, graph_t(size), graphs);  
 
     // Second, remove graphs with pendant edges
-    for(list<adjacency_list<> >::iterator i(graphs.begin());i!=graphs.end();) {
+    for(list<graph_t>::iterator i(graphs.begin());i!=graphs.end();) {
       if(!two_connected(*i)) { graphs.erase(i++); }
       else { i++; }
     }
 
     // Third, remove isomorphic graphs (this could be made more efficient)
     unsigned int ngraphs_generated(graphs.size());
-    for(list<adjacency_list<> >::iterator i(graphs.begin());i!=graphs.end();) {
+    for(list<graph_t>::iterator i(graphs.begin());i!=graphs.end();) {
       boolean unique(true);
       unsigned char *k1 = graph_key(*i);
-      list<adjacency_list<> >::iterator j(i);
+      list<graph_t>::iterator j(i);
       for(++j;j!=graphs.end();++j) {
 	unsigned char *k2 = graph_key(*j);
 	if(compare_graph_keys(k1,k2)) {
@@ -150,15 +191,15 @@ int main(int argc, char *argv[]) {
     cerr << "Generated " << ngraphs_generated << " graphs, of which " << graphs.size() << " are unique." << endl;
 
     // Second, we bucket this list according to edge degree sequence
-    hash_map<vector<int>, adjacency_list<>, degree_sequence_hash> buckets;
+    hash_map<vector<int>, graph_t, degree_sequence_hash> buckets;
     hash_set<vector<int>, degree_sequence_hash> keys;
-    for(list<adjacency_list<> >::iterator i(graphs.begin());i!=graphs.end();++i) {    
+    for(list<graph_t>::iterator i(graphs.begin());i!=graphs.end();++i) {    
       keys.insert(degree_sequence(*i));
       buckets.insert(make_pair(degree_sequence(*i),*i));
     }
     cerr << "Partitioned unique graphs into " << keys.size() << " distinct classes using degree sequences." << endl;   
-    
-    cout << "// THIS CODE WAS AUTOMATICALLY GENERATED" << endl;
+    time_t ct = time(NULL);
+    cout << "// THIS CODE WAS AUTOMATICALLY GENERATED ON " << ctime(&ct) << endl;
     cout << endl;
     string cnm[] = {"ZERO","ONE","TWO","THREE","FOUR","FIVE","SIX","SEVEN","EIGHT","NINE"};
     cout << "#ifndef EVALUATE_SIMPLE_" << cnm[size] << "_HPP" << endl;
@@ -170,11 +211,14 @@ int main(int argc, char *argv[]) {
     cout << "P const &evaluate_simple_" << nm[size] << "(G const &graph) {" << endl << endl;
     cout << "\t// first, define polynomial solutions" << endl;
     for(hash_set<vector<int>,  degree_sequence_hash>::iterator i(keys.begin());i!=keys.end();++i) {
+      graph_t const &graph = buckets.find(*i)->second;
       cout << "\tstatic P poly";
       for(vector<int>::const_iterator j(i->begin());j!=i->end();++j) {
 	cout << "_" << *j;
       }
-      cout << " = P(0,0);" << endl;
+      cout << " = "; 
+      print_graph_poly(graph);
+      cout << ";" << endl;
     }
     cout << endl << "\t// second, compute degree sequence" << endl;
     cout << "\tunsigned long degseq = 0;" << endl;
@@ -188,7 +232,7 @@ int main(int argc, char *argv[]) {
     
     // now, go through the keys and put in the answer for each
     for(hash_set<vector<int>,  degree_sequence_hash>::iterator i(keys.begin());i!=keys.end();++i) {
-      adjacency_list<> const &graph = buckets.find(*i)->second;
+      graph_t const &graph = buckets.find(*i)->second;
       unsigned long k = degree_sequence_key(graph,shift);
       vector<int> ds = degree_sequence(graph);
       cout << "\t\tcase " << k << "L:" << endl;
@@ -203,7 +247,7 @@ int main(int argc, char *argv[]) {
       cout << endl << "\t\t // GRAPH : ";
       bool first_time(true);
       for(unsigned int j=0;j!=graph.num_vertices();++j) {
-	for(adjacency_list<>::edge_iterator k(graph.begin_edges(j));
+	for(graph_t::edge_iterator k(graph.begin_edges(j));
 	    k!=graph.end_edges(j);++k) {
 	  if(j <= *k) {
 	    if(!first_time) { cout << ", "; }
