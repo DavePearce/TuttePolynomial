4c0bca77515cda60eed45e8966f1c5dedb8785c2
DavePearce <david.pearce@ecs.vuw.ac.nz>
2007-01-15 03:44:15 +0000

    Small Graph Solution generator is now working fairly nicely and it's telling me how well degree sequence distinctuishes graphs.
    git-svn-id: svn+ssh://circa.ecs.vuw.ac.nz/u/staff/djp/projects/repository/tuttepoly/trunk@821 cab769db-1af3-0310-8bf3-f2d376eea66d

--- src/misc/sg_gen.cpp
+++ src/misc/sg_gen.cpp
@@ -2,11 +2,16 @@
 // This is the small graph generator, which automatically generates
 // code for matching graphs of a particular size.
 
+#include "nauty.h"
 #include <iostream>
 #include <vector>
+#include <ext/hash_map>
+#include <ext/hash_set>
 #include "../graph/adjacency_list.hpp"
+#include "../graph/algorithms.hpp"
 
 using namespace std;
+using namespace __gnu_cxx;
 
 vector<vector<int> > combinator(int N, int min, int max) {
   bool finished;
@@ -62,6 +67,28 @@ void generate_all_graphs(int v, int V, adjacency_list<> graph, vector<adjacency_
   }
 }
 
+class degree_sequence_hash {
+public:
+  unsigned int operator()(vector<int> const &vec) const {
+    unsigned int r = 0;
+    for(vector<int>::const_iterator i(vec.begin());i!=vec.end();i++) {
+      r ^= *i;
+    }
+    return r;
+  }
+};
+
+vector<int> degree_sequence(adjacency_list<> const &graph) {
+  vector<int> r(graph.num_vertices());
+  for(int i=0;i!=r.size();++i) { r[i] = 0; }
+
+  for(int i=0;i!=r.size();++i) { 
+    r[graph.num_edges(i)]++;
+  }
+
+  return r;
+}
+
 int main(int argc, char *argv[]) {
   bool simple=true;
   long size = strtol(argv[1],NULL,10);
@@ -69,10 +96,45 @@ int main(int argc, char *argv[]) {
 
   try {
     
+    // First, we generate the set of all unique graphs
     vector<adjacency_list<> > graphs;
-    
     generate_all_graphs(0,size, adjacency_list<>(size), graphs);  
+
+    // NEED TO REMOVE THOSE WITH PENDANT EDGES!
     
+    // Second, remove isomorphics graphs
+    vector<adjacency_list<> > unique_graphs;    
+    for(int i=0;i!=graphs.size();++i) {
+      boolean unique(true);
+      adjacency_list<> const &g1(graphs[i]);
+      unsigned char *k1 = graph_key(g1);
+      for(int j=0;j!=unique_graphs.size();++j) {
+	adjacency_list<> const &g2(unique_graphs[j]);
+	unsigned char *k2 = graph_key(g2);
+	if(compare_graph_keys(k1,k2)) {
+	  // this graph is not unique
+	  unique=false;
+	  break;
+	}
+	delete [] k2;
+      }
+      delete [] k1;
+      if(unique) { unique_graphs.push_back(g1); }
+    }
+
+    cout << "Generated " << graphs.size() << " graphs, of which " << unique_graphs.size() << " are unique." << endl;
+
+    // Second, we bucket this list according to edge degree sequence
+    hash_map<vector<int>, adjacency_list<>, degree_sequence_hash> buckets;
+    hash_set<vector<int>, degree_sequence_hash> keys;
+    for(int i=0;i!=graphs.size();++i) {
+      keys.insert(degree_sequence(graphs[i]));
+      buckets.insert(make_pair(degree_sequence(graphs[i]),graphs[i]));
+    }
+    cout << "Partitioned unique graphs into " << keys.size() << " distinct classes." << endl;   
+
+
+    /*
     for(int i=0;i!=graphs.size();++i) {
       cout << "GRAPH #" << i << " : { ";
       adjacency_list<> g(graphs[i]);
@@ -85,6 +147,7 @@ int main(int argc, char *argv[]) {
       }
       cout << " } " << endl;
     }
+    */
   } catch(bad_alloc const &e) {
     cout << "error: insufficient memory!" << endl;
   } catch(exception const &e) {
