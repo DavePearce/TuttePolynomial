e715cd76901fd1055ca5891e6208a04a56b2904f
DavePearce <david.pearce@ecs.vuw.ac.nz>
2007-01-15 04:28:29 +0000

    Working on generating the code!
    git-svn-id: svn+ssh://circa.ecs.vuw.ac.nz/u/staff/djp/projects/repository/tuttepoly/trunk@822 cab769db-1af3-0310-8bf3-f2d376eea66d

--- src/misc/sg_gen.cpp
+++ src/misc/sg_gen.cpp
@@ -5,6 +5,7 @@
 #include "nauty.h"
 #include <iostream>
 #include <vector>
+#include <string>
 #include <ext/hash_map>
 #include <ext/hash_set>
 #include "../graph/adjacency_list.hpp"
@@ -46,7 +47,7 @@ vector<vector<int> > combinator(int N, int min, int max) {
 
 
 // this function recursively generates the list of all possible graphs
-void generate_all_graphs(int v, int V, adjacency_list<> graph, vector<adjacency_list<> > &graphs) {  
+void generate_all_graphs(int v, int V, adjacency_list<> graph, list<adjacency_list<> > &graphs) {  
   if(v == (V-1)) {
     // this is the base case --- there are no more edges to add!
     graphs.push_back(graph);    
@@ -89,6 +90,15 @@ vector<int> degree_sequence(adjacency_list<> const &graph) {
   return r;
 }
 
+bool two_connected(adjacency_list<> const &g) {
+  for(int i=0;i!=g.num_vertices();++i) {
+    if(g.num_edges(i) < 2) {
+      return false;
+    }
+  }
+  return true;
+}
+
 int main(int argc, char *argv[]) {
   bool simple=true;
   long size = strtol(argv[1],NULL,10);
@@ -97,20 +107,23 @@ int main(int argc, char *argv[]) {
   try {
     
     // First, we generate the set of all unique graphs
-    vector<adjacency_list<> > graphs;
+    list<adjacency_list<> > graphs;
     generate_all_graphs(0,size, adjacency_list<>(size), graphs);  
 
-    // NEED TO REMOVE THOSE WITH PENDANT EDGES!
-    
-    // Second, remove isomorphics graphs
-    vector<adjacency_list<> > unique_graphs;    
-    for(int i=0;i!=graphs.size();++i) {
+    // Second, remove graphs with pendant edges
+    for(list<adjacency_list<> >::iterator i(graphs.begin());i!=graphs.end();) {
+      if(!two_connected(*i)) { graphs.erase(i++); }
+      else { i++; }
+    }
+
+    // Third, remove isomorphic graphs (this could be made more efficient)
+    unsigned int ngraphs_generated(graphs.size());
+    for(list<adjacency_list<> >::iterator i(graphs.begin());i!=graphs.end();) {
       boolean unique(true);
-      adjacency_list<> const &g1(graphs[i]);
-      unsigned char *k1 = graph_key(g1);
-      for(int j=0;j!=unique_graphs.size();++j) {
-	adjacency_list<> const &g2(unique_graphs[j]);
-	unsigned char *k2 = graph_key(g2);
+      unsigned char *k1 = graph_key(*i);
+      list<adjacency_list<> >::iterator j(i);
+      for(++j;j!=graphs.end();++j) {
+	unsigned char *k2 = graph_key(*j);
 	if(compare_graph_keys(k1,k2)) {
 	  // this graph is not unique
 	  unique=false;
@@ -119,35 +132,40 @@ int main(int argc, char *argv[]) {
 	delete [] k2;
       }
       delete [] k1;
-      if(unique) { unique_graphs.push_back(g1); }
+      if(!unique) { graphs.erase(i++); }
+      else { i++; }
     }
 
-    cout << "Generated " << graphs.size() << " graphs, of which " << unique_graphs.size() << " are unique." << endl;
+    cout << "Generated " << ngraphs_generated << " graphs, of which " << graphs.size() << " are unique." << endl;
 
     // Second, we bucket this list according to edge degree sequence
     hash_map<vector<int>, adjacency_list<>, degree_sequence_hash> buckets;
     hash_set<vector<int>, degree_sequence_hash> keys;
-    for(int i=0;i!=graphs.size();++i) {
-      keys.insert(degree_sequence(graphs[i]));
-      buckets.insert(make_pair(degree_sequence(graphs[i]),graphs[i]));
+    for(list<adjacency_list<> >::iterator i(graphs.begin());i!=graphs.end();++i) {    
+      keys.insert(degree_sequence(*i));
+      buckets.insert(make_pair(degree_sequence(*i),*i));
     }
-    cout << "Partitioned unique graphs into " << keys.size() << " distinct classes." << endl;   
+    cout << "Partitioned unique graphs into " << keys.size() << " distinct classes using degree sequences." << endl;   
 
+    cout << "-------- CODE STARTS HERE -------- " << endl;
+    
+    cout << "// THIS CODE WAS AUTOMATICALLY GENERATED" << endl;
+    cout << endl;
+    cout << "template<class G, class P>" << endl;
+    string nm[] = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+    cout << "P const &evaluate_simple_" << nm[size] << "(G const &graph) {" << endl << endl;
+    cout << "\t// first, compute degree sequence" << endl;
+    cout << "\tlong degseq = 0;" << endl;
+    cout << "\tfor(typename G::vertex_iterator i(graph.begin_verts());i!=graph.end_verts();++i) { " << endl;
+    cout << "\t\tdeg[graph.num_edges(*i)]++;" << endl;
+    cout << "\t}" << endl;
+    cout << endl;
+    cout << "\t// now, switch on it!" << endl;
+    cout << "\t long 
+    
+    
+    // now, print code for matching graphs!
 
-    /*
-    for(int i=0;i!=graphs.size();++i) {
-      cout << "GRAPH #" << i << " : { ";
-      adjacency_list<> g(graphs[i]);
-      for(adjacency_list<>::vertex_iterator i(g.begin_verts());i!=g.end_verts();++i) {
-	for(adjacency_list<>::edge_iterator j(g.begin_edges(*i));j!=g.end_edges(*i);++j) {
-	  if(*i <= *j) {
-	    cout << *i << "--" << *j << " ";
-	  } 
-	}
-      }
-      cout << " } " << endl;
-    }
-    */
   } catch(bad_alloc const &e) {
     cout << "error: insufficient memory!" << endl;
   } catch(exception const &e) {
