05a71e79dd39ae66c3bbc2c584b0bbbe6005f422
DavePearce <david.pearce@ecs.vuw.ac.nz>
2007-02-21 03:44:39 +0000

    Added file for new poly representation.
    git-svn-id: svn+ssh://circa.ecs.vuw.ac.nz/u/staff/djp/projects/repository/tuttepoly/trunk@855 cab769db-1af3-0310-8bf3-f2d376eea66d

---
+++ src/poly/factor_poly.hpp
@@ -0,0 +1,19 @@
+#ifndef SIMPLE_POLY_HPP
+#define SIMPLE_POLY_HPP
+
+#include "xy_term.hpp"
+
+class factor_poly {
+private:
+  typedef struct {
+    unsigned short ypower;
+    unsigned short ypowerend;
+  } y_term;
+  
+  y_term* terms;
+
+public:
+  
+}
+
+#endif
