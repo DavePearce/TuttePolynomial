e46fa100c8e048b166c4baad928ac58cad4128de
DavePearce <david.pearce@ecs.vuw.ac.nz>
2007-02-21 04:59:19 +0000

    very little work on factor poly!
    git-svn-id: svn+ssh://circa.ecs.vuw.ac.nz/u/staff/djp/projects/repository/tuttepoly/trunk@856 cab769db-1af3-0310-8bf3-f2d376eea66d

--- src/poly/factor_poly.hpp
+++ src/poly/factor_poly.hpp
@@ -10,10 +10,19 @@ private:
     unsigned short ypowerend;
   } y_term;
   
-  y_term* terms;
-
+  unsigned int** terms; // first term is y_term
+  unsigned int nxterms;
 public:
   
+  factor_poly(xy_term const &t) {
+    nxterms = t.xpower;
+    // create the xterm array
+    terms = new (unsigned int*)[nxterms];
+    for(unsigned int i=0;i!=nxterms;++i) {
+      terms[i] = NULL;
+    }
+    
+  }
 }
 
 #endif
