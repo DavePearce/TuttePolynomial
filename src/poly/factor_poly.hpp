15517aa88790c0361055a0164915e7f53e174cf1
DavePearce <david.pearce@ecs.vuw.ac.nz>
2007-04-24 05:06:39 +0000

    working on making the factor poly templated on the coefficient type.
    git-svn-id: svn+ssh://circa.ecs.vuw.ac.nz/u/staff/djp/projects/repository/tuttepoly/trunk@944 cab769db-1af3-0310-8bf3-f2d376eea66d

--- src/poly/factor_poly.hpp
+++ src/poly/factor_poly.hpp
@@ -6,49 +6,245 @@
 #include <utility>
 #include "xy_term.hpp"
 
+#define FPOLY_PADDING_FACTOR 1
+
+template<class T>
 class yterms {
-private:
-  typedef struct header {
-    unsigned int ymin;
-    unsigned int ymax;
-    unsigned int front_padding;
-    unsigned int back_padding;
-  } header;
+public:  
+  unsigned int ymin;
+  unsigned int ymax;
+private:  
+  unsigned int fpadding;
+  unsigned int bpadding;
+  T *coefficients;
 public:
-  header *ptr; // header first, then front padding, followed by data, then back padding
-public:
-  yterms();
-  yterms(unsigned int y_min, unsigned int y_max);
-  yterms(yterms const &src);
+  /* =============================== */
+  /* ========= CONSTRUCTORS ======== */
+  /* =============================== */
+
+  yterms() : coefficients(NULL) { };
+
+  yterms(unsigned int y_min, unsigned int y_max) {
+    alloc(y_min,y_max);
+  }
+
+  yterms(yterms<T> const &src)  { clone(src); }
+
   ~yterms();
 
-  void resize(unsigned int y_min, unsigned int y_max);
-  void swap(yterms &src);
-  unsigned int size() const;
-  unsigned int ymin() const { return ptr->ymin; }
-  unsigned int ymax() const { return ptr->ymax; }
-  bool is_empty() const;  
+  /* =============================== */
+  /* ======== ASSIGNMENT OP ======== */
+  /* =============================== */
 
-  yterms const &operator=(yterms const &src);
-  void operator+=(xy_term const &p);
-  void operator+=(yterms const &src);
-  void insert(unsigned int n, xy_term const &p); // needs a better name? 
-  void operator*=(xy_term const &p);
-  unsigned int operator[](int) const;
-  unsigned int nterms() const;
+  yterms const &operator=(yterms<T> const &src) {
+    if(&src != this) {
+      delete [] coefficients;
+      clone(src);
+    }
+    return *this;
+  }
 
-  double substitute(double y) const;
-  std::string str() const;
+  void yterms::swap(yterms<T> &src) {
+    if(&src != this) {
+      std::swap(ymin,src.ymin);
+      std::swap(ymax,src.ymax);
+      std::swap(fpadding,src.fpadding);
+      std::swap(bpadding,src.bpadding);
+      std::swap(coefficients,src.coefficients);
+    }
+  }
+
+  /* =============================== */
+  /* ======== ARITHMETIC OPS ======= */
+  /* =============================== */
+
+  void operator+=(xy_term const &p) {
+    // make sure enough y terms
+    resize(p.ypower,p.ypowerend);
+    // now, do the addition
+    for(unsigned int i=p.ypower;i<=p.ypowerend;++i) { 
+      coefficients[i - ymin + fpadding] += 1;
+    }    
+  }
+
+  void operator+=(yterms<T> const &src) {
+    // make sure enough y terms
+    resize(src.ptr->ymin,src.ptr->ymax);
+    // now, do the addition
+    unsigned int start = src.ptr->ymin;
+    unsigned int end = src.ptr->ymax;
+    for(unsigned int i=start;i<=end;++i) { 
+      T c(src.coefficients[i - src.ymin + src.fpadding]);
+      coefficients[i - ymin + fpadding] += c;
+    }
+  }
+
+  void operator*=(xy_term const &p) {
+    // if this poly is empty do nothing!
+    if(is_empty()) { return;}
+    // Ok, it's not empty ...
+    unsigned int ystart = ymin;
+    unsigned int yend = ymax;
+    unsigned int ypadding = fpadding;
+    unsigned int nystart = ymin + p.ypower;
+    unsigned int nyend = ymax + p.ypowerend;
+    
+    if(p.ypower == p.ypowerend) {
+      // easy case, only a shift required
+      ymin = nystart;
+      ymax = nyend;
+    } else {
+      // harder case
+      
+      // The following could use padding, since it currently doesn't!
+      T *o_coefficients = coefficients;
+      alloc(nystart,nyend);     
+      unsigned int depth = (p.ypowerend-p.ypower)+1;
+      unsigned int width = (yend-ystart)+1;
+      
+      // going up the triangle
+      T acc = 0;    
+      for(unsigned int i=0;i<min(width,depth);++i) {
+	acc += o_coefficients[i+ypadding];
+	coefficients[i+nystart+fpadding-ymin] = acc;
+      }
+      // free fall (if there is any)
+      for(unsigned int i=width;i<depth;++i) {
+	coefficients[i+nystart+fpadding-ymin] = acc;
+      }    
+      // going along the top (if there is one)
+      T sub = 0;
+      for(unsigned int i=depth;i<width;++i) {
+	sub += o_coefficients[i+ypadding-depth];
+	acc += o_coefficients[i+ypadding];
+	coefficients[i+fpadding] = acc - sub;
+      }    
+
+      // going down the triangle
+      for(unsigned int i=max(depth,width);i < (nyend-nystart)+1;++i) {
+	sub += o_coefficients[i+ypadding - depth];
+	coefficients[i+fpadding] = acc - sub;
+      }
+      delete [] o_coefficients;
+    }
+    // and we're done!
+  }
+
+  /* ========================== */
+  /* ======== OTHER FNS ======= */
+  /* ========================== */  
+
+  unsigned int size() const {
+    if(coefficients == NULL) { return 0; }
+    return (ymax - ymin) + 1;
+  }
+
+  bool is_empty() const { return ptr == NULL; }
+
+  T operator[](int) const { return coefficients[i + fpadding - ymin]; }
+
+  double substitute(double y) const {
+    return 0.0;
+  }
+
+  std::string str() const {
+    std::stringstream ss;
+    if(ymin != ymax) {
+      ss << "y^{" << ymin << ".." << ymax << "}";
+    } else if(ymin == 1) {
+      ss << "y";
+    } else if(ymin != 0) {
+      ss << "y^" << ymin;
+    } else if(ymin == 0) {
+      return "";
+    }
+    ss << "(";
+    for(unsigned int i=ymin;i<=ymax;++i) {
+      if(i != ymin) { ss << " + "; }
+      ss << coefficients[i + fpadding - ymin];
+    }
+    ss << ")";
+    return ss.str();
+  }
+
+  void insert(unsigned int n, xy_term const &p) {   // needs a better name? 
+    // make sure enough y terms
+    resize(p.ypower,p.ypowerend);
+    // now, do the addition
+    for(unsigned int i=p.ypower;i<=p.ypowerend;++i) { 
+      coefficients[i + fpadding - ymin] += n;
+    }    
+  }
+  
 private:  
-  void clone(yterms const &src);  
-  // low level operations
-  unsigned int get(unsigned int y, header *h) const;
-  void set(unsigned int y, unsigned int v, header *h);
-  void add(unsigned int y, unsigned int v, header *h);
-  header *alloc(unsigned int ymin, unsigned int ymax);
-  void unalloc(header *ptr);
+  /* =============================== */
+  /* ======== HELPERS ======= */
+  /* =============================== */
+
+  void resize(unsigned int y_min, unsigned int y_max)  {
+    if(is_empty()) {
+      // special case when optr == NULL
+      alloc(y_min,y_max);
+    } else {
+      int d_end = y_max - ptr->ymax;
+      int d_beg = ptr->ymin - y_min;
+      
+      if(d_beg <= (int) fpadding && d_end <= (int) bpadding) {
+	// in this case, there is enough padding to cover it
+	if(d_end > 0) {
+	  ymax = y_max;      
+	  bpadding -= d_end;
+	}
+	if(d_beg > 0) {
+	  ymin = y_min;      
+	  fpadding -= d_beg;
+	}
+      } else {
+	// no, there definitely aren't enough y-terms
+	unsigned int ystart = ymin;
+	unsigned int yend = ymax;
+	unsigned int ypadding = fpadding;
+	T *o_coefficients = coefficients;	
+	ptr = alloc(min(ystart,y_min),max(yend,y_max));
+	// copy old stuff over
+	for(unsigned int i=ystart;i<=yend;++i) { 
+	  coefficients[i+fpadding-ymin] = o_coefficients[i+ypadding-ystart];
+	}
+	// free space!
+	delete [] o_coefficients;
+      }
+    }
+  }  
+
+  void clone(yterms<T> const &src) { 
+    if(src.is_empty()) { coefficients = NULL; }
+    else {
+      ymin = src.ymin;
+      ymax = src.ymax;
+      fpadding = src.fpadding;
+      bpadding = src.bpadding;
+
+      unsigned int ncoeffs = src.size() + src.fpadding + src.bpadding;
+      
+      coefficients = new T[ncoeffs];
+
+      // copy old stuff over
+      for(unsigned int i=ystart;i<=yend;++i) { 
+	coefficients[i+fpadding-ymin] = src.coefficients[i+fpadding-ymin];
+      }
+    }
+  }
+
+  void alloc(unsigned int _ymin, unsigned int _ymax) {
+    unsigned int nyterms = (_ymax-_ymin)+1;
+    bpadding = nyterms * FPOLY_PADDING_FACTOR;
+    fpadding = min(_ymin,nyterms * FPOLY_PADDING_FACTOR);
+    ymin = _ymin;
+    ymax = _ymax;
+  }
 };
 
+/*
 // This iterator is a temporary structure
 class factor_poly_iterator {
 private:
@@ -128,5 +324,5 @@ private:
   void clone(factor_poly const &p);
   void resize_xterms(unsigned int ns);
 };
-
+*/
 #endif
