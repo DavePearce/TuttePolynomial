a9d6e686d3e3290fda3ac805351567790849facf
DavePearce <david.pearce@ecs.vuw.ac.nz>
2007-05-06 08:51:40 +0000

    More changes.
    git-svn-id: svn+ssh://circa.ecs.vuw.ac.nz/u/staff/djp/projects/repository/tuttepoly/trunk@955 cab769db-1af3-0310-8bf3-f2d376eea66d

--- src/misc/bstreambuf.hpp
+++ src/misc/bstreambuf.hpp
@@ -3,27 +3,53 @@
 
 class bstreambuf {
 private:
-  char *buffer;
-  unsigned int max;
-  unsigned int ptr;
+  char *start;
+  char *end;
+  char *ptr;
 public:
   bstreambuf() { this(1024); }
 
   bstreambuf(unsigned int m) {
-    max = m;
-    ptr = 0;
-    buffer = new char[m];
+    start = new char[m];
+    ptr = start;
+    max = start + m;
   }
 
   bstreambuf(bstreambuf const &src) {
-    max = src.max;
-    ptr = src.ptr;
-    buffer = new char[max];
-    memcpy(buffer,src.buffer,max);
+    start = new char[src.max()];
+    memcpy(start,src.buffer,src.size());
   }
 
   bstream const &operator=(bstreambuf const &src) {
+    if(this != &src) {
+      delete [] start;
+      end = src.end;
+      ptr = src.ptr;
+      start = new char[src.max()];
+      memcpy(buffer,src.buffer,src.size());      
+    }
+  }
 
+  void write(unsigned int v) {
+    if((ptr+sizeof(v)) >= end) {
+      resize(ptr + sizeof(unsigned int));      
+    } 
+    // this line is a bit of a hack
+    *((unsigned int*) ptr) = v;
+    ptr += sizeof(unsigned int);
+  }
+
+  unsigned int size() const { return ptr-start; }
+  unsigned int max() const { return end-start; }
+
+private:
+  // resize so we have at least min bytes
+  void resize(unsigned int min) {
+    unsigned int nmax = std::max(min,2*max());
+    char *nstart = new char[nmax];
+    memcpy(nbuf,start,size());
+    
+    
   }
 }
 
