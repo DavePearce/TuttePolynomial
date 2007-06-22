ba8cbd44cedb9ca77e5029c87e7bc46ef9346ff5
DavePearce <david.pearce@ecs.vuw.ac.nz>
2007-06-22 23:22:54 +0000

    Isolated the bug in tutte.cpp now.  Basically, it definitely occurs when multiplying 21, when represented by 21,0 in the biguint array.  During the computation, this can occur after substraction operations.
    git-svn-id: svn+ssh://circa.ecs.vuw.ac.nz/u/staff/djp/projects/repository/tuttepoly/trunk@1005 cab769db-1af3-0310-8bf3-f2d376eea66d

--- src/tutte.cpp
+++ src/tutte.cpp
@@ -728,6 +728,22 @@ int main(int argc, char *argv[]) {
     }    
   }
 
+  biguint x(21U);
+  biguint y(UINT_MAX);
+  x += y;
+  cout << "X = " << x << endl;
+  x -= y;
+  if(x.ptr & BUI_PTR_BIT) {
+    std::cout << "(";
+    bui_word *ptr = UNPACK(x.ptr);
+    for(bui_word j(0);j!=ptr[0];++j) {
+      cout << ptr[j+1] << " ";
+    }
+    cout << ") ";
+  }
+  cout << "X = " << x << endl;
+  cout << "GOT: " << biguint(pow(biguint(2U),31) * x) << endl;
+
   // Quick sanity check
 
   if(optind >= argc) {
