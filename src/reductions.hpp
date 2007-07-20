84aef7d58c6607543ea5f4cf9c58e67a7585b9ce
DavePearce <david.pearce@ecs.vuw.ac.nz>
2007-07-20 06:04:59 +0000

    In the process of updating the pendant edge removal system
    git-svn-id: svn+ssh://circa.ecs.vuw.ac.nz/u/staff/djp/projects/repository/tuttepoly/trunk@1042 cab769db-1af3-0310-8bf3-f2d376eea66d

--- src/reductions.hpp
+++ src/reductions.hpp
@@ -5,12 +5,34 @@
 template<class G>
 unsigned int remove_pendants(G &graph) {
   unsigned int num_pendants(0);
+  std::vector<unsigned int> pendants;
+  
+  for(typename G::vertex_iterator i(graph.begin_verts());i!=graph.end_verts();++i) {
+    // change to num_underlying_edges for multi_edge removal
+    if(graph.num_edges(*i) == 1) {
+      pendants.push_back(*i);
+    }
+  }
+  
+  while(pendants.size() > 0) {
+    unsigned int p(pendants.back());
+    pendants.pop_back();
+    num_pendants++;
+    typename G::edge_t e(*graph.begin_edges(p));
+    graph.remove(p);
+    if(graph.num_edges( ... edge thing goes here ... ) == 1) {
+
+    }
+  }
+
+  /*
   while(graph.num_pendant_vertices() > 0) {
     unsigned int l = graph.select_pendant_vertex();
     graph.remove(l);
     num_pendants++;
   }
   return num_pendants;
+  */
 }
 
 #endif
