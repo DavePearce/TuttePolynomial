379d0a16ae034e334b70626872c610214df16985
DavePearce <david.pearce@ecs.vuw.ac.nz>
2007-07-26 00:07:25 +0000

    Trying to tidy up the reduction code a bit ...
    git-svn-id: svn+ssh://circa.ecs.vuw.ac.nz/u/staff/djp/projects/repository/tuttepoly/trunk@1056 cab769db-1af3-0310-8bf3-f2d376eea66d

--- src/reductions.hpp
+++ src/reductions.hpp
@@ -2,39 +2,52 @@
 #define REDUCTIONS_HPP
 
 #include <stdexcept>
+#include <vector>
+#include "misc/triple.hpp"
+
+typedef triple<unsigned int, unsigned int, unsigned int> edge_t;
+typedef std::vector<edge_t> line_t;
 
 template<class G, class P>
-xy_term reduce_pendants(G &graph) {
-  unsigned int num_pendants(0);
-
-  std::vector<unsigned int> pendants;
+P reduce_pendant(unsigned int p, G &graph) {
+  unsigned int count(graph.num_edges(p));
+  graph.remove(p);
   
-  for(typename G::vertex_iterator i(graph.begin_verts());i!=graph.end_verts();++i) {
-    if(graph.num_edges(*i) == 1) {
-      pendants.push_back(*i);
-    }
-  }
+  P r = X(1);
+  if(count > 1) { r += Y(1,count-1); }
+  return r;
+}
 
-  while(pendants.size() > 0) {
-    unsigned int p(pendants.back());
-    pendants.pop_back();
-    num_pendants++;
-    // now, recursively eliminate any pendants created by this
-    typename G::edge_iterator i(graph.begin_edges(p));
-    unsigned int w(i->first);
-    graph.remove(p);
-    if(graph.num_edges(w) == 1) { pendants.push_back(w); } 
-  }
+template<class G, class P>
+P reduce_cycle(line_t const &line, G &graph) {
+  // This is a somewhat icky piece of code for reducing 
+  // a cycle.
 
-  /*
-  while(graph.num_pendant_vertices() > 0) {
-    unsigned int l = graph.select_pendant_vertex();
-    graph.remove(l);
-    num_pendants++;
+  for(unsigned int j=0;j!=line.size()-1;++j) {
+    graph.remove(line[j].second);
   }
-  */
+    
+  P xs(X(1)), acc(X(1));
+  if(line[0].third > 1) { 
+    acc += Y(1,line[0].third-1); 
+    xs += Y(1,line[0].third-1); 
+  }
+      
+  for(unsigned int k=1;k<line.size()-1;++k) {
+    P tmp(X(1));
+    if(line[k].third > 1) { tmp += Y(1,line[k].third-1); }
+    if(line[k+1].third > 1) { xs *= Y(0,line[k+1].third-1); }
+    acc *= tmp;
+    xs += acc;
+  }      
+  
+  P ys = Y(line[0].third,line[0].third);
+  for(unsigned int k=1;k<line.size();++k) {
+    if(line[k].third > 1) { ys *= Y(0,line[k].third-1); }
+  }
+  xs += ys;
 
-  return X(num_pendants);
+  return xs;
 }
 
 template<class G, class P>
@@ -57,12 +70,8 @@ P reduce_multi_pendants(G &graph) {
     // Generate multiply factor
     typename G::edge_iterator i(graph.begin_edges(p));
     unsigned int w(i->first);
-    unsigned int count(i->second);
-
-    P tmp(X(1));
-    if(count > 1) {  tmp += Y(1,count-1); }
-    r *= tmp;
-    graph.remove(p);
+    
+    r *= reduce_pendant<G,P>(p,graph);
     // Recursively eliminate any pendants created by this
     if(graph.num_underlying_edges(w) == 1) { 
       pendants.push_back(w); } 
@@ -72,9 +81,9 @@ P reduce_multi_pendants(G &graph) {
 }
 
 template<class G>
-std::vector<triple<unsigned int, unsigned int, unsigned int> > trace_line(unsigned int v, G const &graph) {  
+line_t trace_line(unsigned int v, G const &graph) {  
   // This is a crude, O(v) time algorithm for tracing out a line in the graph.
-  std::vector<triple<unsigned int, unsigned int, unsigned int> > line;
+  line_t line;
   unsigned int start=v; // needed to protect against cycles
 
   // First, find one end of the line  
@@ -131,7 +140,7 @@ P reduce_cycles(G &graph) {
 
     if(visited[c]) { continue; }
         
-    std::vector<triple<unsigned int, unsigned int, unsigned int> > line = trace_line(c,graph);
+    line_t line = trace_line(c,graph);
     
     // mark whole line as visited
     for(unsigned int j=0;j!=line.size()-1;++j) {
@@ -139,32 +148,8 @@ P reduce_cycles(G &graph) {
     }
     
     if(line[0].first == line[line.size()-1].second) {      
-      // Found a cycle!
-      for(unsigned int j=0;j!=line.size()-1;++j) {
-	graph.remove(line[j].second);
-      }
-
-      // Now, build its factor!
-      P xs(X(1)), acc(X(1));
-      if(line[0].third > 1) { 
-	acc += Y(1,line[0].third-1); 
-	xs += Y(1,line[0].third-1); 
-      }
       
-      for(unsigned int k=1;k<line.size()-1;++k) {
-	P tmp(X(1));
-	if(line[k].third > 1) { tmp += Y(1,line[k].third-1); }
-	if(line[k+1].third > 1) { xs *= Y(0,line[k+1].third-1); }
-	acc *= tmp;
-	xs += acc;
-      }      
-      
-      P ys = Y(line[0].third,line[0].third);
-      for(unsigned int k=1;k<line.size();++k) {
-	if(line[k].third > 1) { ys *= Y(0,line[k].third-1); }
-      }
-      xs += ys;
-      r *= xs;
+      r *= reduce_cycle<G,P>(line,graph);
 
       // Finally, check if this has exposed another candidate
       if(graph.num_underlying_edges(line[0].first) == 2) {
