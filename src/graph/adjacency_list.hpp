f8e0db830d0f0ff956361d03c190fb9c8fdefcb6
DavePearce <david.pearce@ecs.vuw.ac.nz>
2007-08-20 00:28:38 +0000

    Upgraded files to include proper licensing.
    git-svn-id: svn+ssh://circa.ecs.vuw.ac.nz/u/staff/djp/projects/repository/tuttepoly/trunk@1122 cab769db-1af3-0310-8bf3-f2d376eea66d

--- src/graph/adjacency_list.hpp
+++ src/graph/adjacency_list.hpp
@@ -1,3 +1,11 @@
+// (C) Copyright David James Pearce and Gary Haggard, 2007. 
+// Permission to copy, use, modify, sell and distribute this software 
+// is granted provided this copyright notice appears in all copies. 
+// This software is provided "as is" without express or implied 
+// warranty, and with no claim as to its suitability for any purpose.
+//
+// Email: david.pearce@mcs.vuw.ac.nz
+
 #ifndef ADJACENCY_LIST_HPP
 #define ADJACENCY_LIST_HPP
 
