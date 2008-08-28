52f0f77cca2a431cb033cc562f6a2368182f8aaf
DavePearce <david.pearce@ecs.vuw.ac.nz>
2008-08-28 22:30:06 +0000

    Bug fix thanks to mudflap.
    git-svn-id: svn+ssh://circa.ecs.vuw.ac.nz/u/staff/djp/projects/repository/tuttepoly/trunk@1289 cab769db-1af3-0310-8bf3-f2d376eea66d

--- tutte/tutte.cpp
+++ tutte/tutte.cpp
@@ -412,7 +412,7 @@ P tutte(G &graph, unsigned int mid) {
     if(cache.lookup(key,r,match_id)) { 
       if(write_tree) { write_tree_match(mid,match_id,graph,cout); }
       delete [] key; // free space used by key
-      cache_hit_sizes[graph.num_vertices()+1]++;
+      cache_hit_sizes[graph.num_vertices()]++;
       return r * RF;
     }
   }
